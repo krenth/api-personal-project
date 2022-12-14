0ba071d47f610296068945c036f6f60e2d394e0a
krenth <krenth@wgu.edu>
2022-12-13 23:04:48 -0700

    Remove Instance of Url and Headers
    Remove first instance of url and header argument
    declerations. When moved to the function that handles
    and runs all functions, the arguments are defined there
    to be within the function scope. First instance of the
    arguments are not needed.

--- api_script.py
+++ api_script.py
@@ -3,11 +3,6 @@ import json
 from plyer import notification
 from playsound import playsound 
 
-stock_analysis_url = "https://upcoming-ipo-calendar.p.rapidapi.com/ipo-calendar"
-stock_analysis_headers = {
-	"X-RapidAPI-Key": "f7b5875d5emsh4d9c9bbb1502f01p10e1f8jsnd8f03f3933ef",
-	"X-RapidAPI-Host": "upcoming-ipo-calendar.p.rapidapi.com"
-}
 
 def get_data(url, headers):
     response = requests.request("GET", stock_analysis_url, headers=headers)
