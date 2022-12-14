5130e57dda3273dc60c33fe19b8700ff3288ee39
krenth <krenth@wgu.edu>
2022-12-13 20:08:47 -0700

    Add Function to Retrieve API Data
    Add function 'get_data' which accepts the parameters
    'url' and 'headers'. These parameters are used to
    access the endpoint for the API from Stockanalysis.
    The function retrieves the data from the API, and
    parses it to JSON, and returns the data to be readable
    for other aspects of the program.

--- api_script.py
+++ api_script.py
@@ -1,1 +1,26 @@
 import requests
+import json
+
+stock_analysis_url = "https://upcoming-ipo-calendar.p.rapidapi.com/ipo-calendar"
+stock_analysis_headers = {
+	"X-RapidAPI-Key": "f7b5875d5emsh4d9c9bbb1502f01p10e1f8jsnd8f03f3933ef",
+	"X-RapidAPI-Host": "upcoming-ipo-calendar.p.rapidapi.com"
+}
+
+def get_data(url, headers):
+    response = requests.request("GET", stock_analysis_Url, headers=headers)
+    data = response.json()
+    data = data['data']
+    return data
+
+def checkData(data):
+    upcoming_bool = false
+    if 'OpenAI' in data:
+        upcoming_bool = true
+    return upcoming_bool
+
+
+
+    
+
+    
